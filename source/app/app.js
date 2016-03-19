a41439c5a2c92b1cb4636fe2a1ab0ec8977e6cae
Eddie Kao <eddie@digik.com.tw>
2016-03-19 22:25:07 +0800

    move main.js to app.js

--- source/app/app.js
+++ source/app/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './source/app/components/hello_world';
+import HelloWorld from './components/hello_world';
 
 ReactDOM.render(<HelloWorld />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   entry: [
-    './main.js'
+    './source/app/app.js'
   ],
 
   output: {
